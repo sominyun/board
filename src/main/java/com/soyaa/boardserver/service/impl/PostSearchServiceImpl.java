d6cde1afc3ac81bf9f906d146576e161439da307
sominyun <somin455@naver.com>
2024-08-23 20:48:31 +0900

    fix: 비동기 추가

--- src/main/java/com/soyaa/boardserver/service/impl/PostSearchServiceImpl.java
+++ src/main/java/com/soyaa/boardserver/service/impl/PostSearchServiceImpl.java
@@ -8,6 +8,7 @@ import com.soyaa.boardserver.service.PostSearchService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class PostSearchServiceImpl implements PostSearchService {
     @Autowired
     private PostSearchMapper productSearchMapper;
 
+    @Async
     @Cacheable(value = "getProducts", key = "'getProducts' + #postSearchRequest.getName() + #postSearchRequest.getCategoryId()")
     @Override
     public List<PostDTO> getProducts(PostSearchRequest postSearchRequest) {
